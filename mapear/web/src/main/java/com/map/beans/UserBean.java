e23068cfa0f829c5efb220d277bfb1dd1bf2b129
diego <diego@192.168.1.98>
2016-07-20 10:06:32 -0500

    test

--- mapear/web/src/main/java/com/map/beans/UserBean.java
+++ mapear/web/src/main/java/com/map/beans/UserBean.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-
+//
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
